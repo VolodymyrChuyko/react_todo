39d9bf3e8e8b7710f88a0ee50df21b38afcbbfec
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-11-07 15:15:55 +0200

    Update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     semi: 0,
     'no-proto': 0,
     'no-unused-vars': 0,
+    'no-shadow': 0,
 
     // TypeScript
     '@typescript-eslint/no-unused-vars': ['error'],
@@ -20,6 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/ban-types': 0,
     '@typescript-eslint/semi': ['error'],
+    'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/control-has-associated-label': 0,
   },
   parser: '@typescript-eslint/parser',
   plugins: [
